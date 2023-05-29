534c6a47c24d681787e5788f1d9cda5f052d927d
Maksim Perov <coder@frtk.ru>
2023-05-29 17:19:39 +0300

    debug mode is added to flask app

--- staffLocation.py
+++ staffLocation.py
@@ -3,24 +3,37 @@
 from flask import Flask, jsonify, render_template, request
 from getLinuxUserLocation import getLocation
 from gevent.pywsgi import WSGIServer
+import inspect
 
 app = Flask(__name__)
 
+#DEBUG=True
+DEBUG=False
+
 @app.route('/location/server1', methods=["POST"])
 def server1():
+    if DEBUG:
+        print(inspect.currentframe().f_code.co_name)
     text = getLocation.show(host='localhost', user='coder', port=22)
     return jsonify(result=text)
 
 @app.route('/location/server2', methods=["POST"])
 def server2():
+    if DEBUG:
+        print(inspect.currentframe().f_code.co_name)
     text = getLocation.show(host='localhost', user='coder', port=1022, debug=True, old=True)
     return jsonify(result=text)
 
 @app.route("/location/", defaults={"js": "server1"})
 @app.route("/location/<any(server1, server2):js>")
 def index(js):
+    if DEBUG:
+        print(inspect.currentframe().f_code.co_name)
     return render_template("/location/" + f"{js}.html", js=js)
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', 8080), app)
-    http_server.serve_forever()
+    if DEBUG:
+        app.run(host='0.0.0.0', port=8080)
+    else:
+        http_server = WSGIServer(('0.0.0.0', 8080), app)
+        http_server.serve_forever()
